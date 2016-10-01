9b40ff3aaf561b50b060f9631699c82d8eefab8b
MrMutt14 <MrMutt14@users.noreply.github.com>
2016-10-01 20:38:14 +0300

    Upload

---
+++ ConsoleFlow/Client/Program.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+namespace Client
+{
+    class Program
+    {
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
+        private const int PORT = 9342;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+
+        static void Main()
+        {
+            Console.Title = "--- * Flow Client * ---";
+            SetupServer();
+            string exiter = Console.ReadLine();
+            if (exiter == "exit")
+                CloseAllSockets();
+        }
+
+        private static void SetupServer()
+        {
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        /// <summary>
+        /// Close all connected client (we do not need to shutdown the server socket as its connections
+        /// are already closed with the clients).
+        /// </summary>
+        private static void CloseAllSockets()
+        {
+            foreach (Socket socket in clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            serverSocket.Close();
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+            clientSockets.Add(socket);
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                // Don't shutdown because the socket may be disposed and its disconnected anyway.
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+
+            if (text.ToLower() == "/time") // Server requested time
+            {
+
+                byte[] data = Encoding.ASCII.GetBytes(DateTime.Now.ToLongTimeString());
+                current.Send(data);
+            }
+            else if (text.ToLower() == "/ip") // Server requested time
+            {
+
+                byte[] data = Encoding.ASCII.GetBytes(GetIP4Address());
+                current.Send(data);
+            }
+            else if (text.ToLower() == "/os") // Server requested time
+            {
+
+                byte[] data = Encoding.ASCII.GetBytes(FriendlyName());
+                current.Send(data);
+            }
+            else if (text.Contains("/msg")) // Server requested time
+            {
+                text = text.Remove(0, 4);
+                Console.WriteLine("You got a message: " + text);
+                byte[] data = Encoding.ASCII.GetBytes("Recieved Message");
+                current.Send(data);
+            }
+            else if (text.ToLower() == "/exit") // Server wants to exit
+            {
+                // Always Shutdown before closing
+                current.Shutdown(SocketShutdown.Both);
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+        #region getinfo
+        public static string HKLM_GetString(string path, string key)
+        {
+            try
+            {
+                RegistryKey rk = Registry.LocalMachine.OpenSubKey(path);
+                if (rk == null) return "";
+                return (string)rk.GetValue(key);
+            }
+            catch { return ""; }
+        }
+
+        public static string FriendlyName()
+        {
+            string ProductName = HKLM_GetString(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ProductName");
+            string CSDVersion = HKLM_GetString(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion", "CSDVersion");
+            if (ProductName != "")
+            {
+                return (ProductName.StartsWith("Microsoft") ? "" : "Microsoft ") + ProductName +
+                            (CSDVersion != "" ? " " + CSDVersion : "");
+            }
+            return "";
+        }
+
+        public static string GetIP4Address()
+        {
+            IPAddress[] ips = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress i in ips)
+            {
+                if (i.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    return i.ToString();
+                }
+            }
+            return "127.0.0.1";
+        }
+        #endregion
+        
+        
+    }
+}
---
+++ ConsoleFlow/ConsoleFlow/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+namespace ConsoleFlow
+{
+    class Program
+    {
+        
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 9342;
+
+        static void Main()
+        {
+            Console.Title = "ConsoleFlow";
+            Console.WriteLine("--Welcome to ConsoleFlow--");
+            Console.WriteLine("a free opensource Console RAT developed by FORC3FI3LD");
+            Console.WriteLine("github.com/MrMutt14/ConsoleFlow");
+            Console.WriteLine("Please donate to keep the develompent of this program! (donation link in github)");
+            Console.WriteLine("");
+            Console.Title = "ConsoleFlow - A Console RAT Developed by FORC3FI3LD";
+            Thread.Sleep(500);
+            ConnectToServer();
+            RequestLoop();
+            //Exit();
+        }
+
+        private static void ConnectToServer()
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine("Connection attempt " + attempts);
+                    // Change IPAddress.Loopback to a remote IP to connect to a remote host.
+                    ClientSocket.Connect(IPAddress.Loopback, PORT);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
+
+        private static void RequestLoop()
+        {
+            Console.WriteLine(@"<Type ""exit"" to properly disconnect client>");
+
+            while (true)
+            {
+                SendRequest();
+                ReceiveResponse();
+            }
+        }
+
+        /// <summary>
+        /// Close socket and exit program.
+        /// </summary>
+        private static void Exit()
+        {
+            SendString("exit"); // Tell the server we are exiting
+            ClientSocket.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        private static void SendRequest()
+        {
+            a:
+            Console.WriteLine("Write /help for help!");
+            Console.WriteLine("");
+            
+            Console.WriteLine("Write a command:");
+            string request = Console.ReadLine();
+            if (request == "/help")
+            {
+                Console.WriteLine("----HELP MENU----");
+                Console.WriteLine("/ip - Gives the clients ip");
+                Console.WriteLine("/time - Gives the UTC time of the client");
+                Console.WriteLine("/os - Gives the OS the client");
+                Console.WriteLine("/msg - Gives the OS the client");
+                goto a;
+            }
+            
+            else
+            {
+                #region actual sending
+                SendString(request);
+
+                if (request.ToLower() == "/exit")
+                {
+                    Exit();
+                }
+                #endregion
+            }
+        }
+
+        /// <summary>
+        /// Sends a string to the server with ASCII encoding.
+        /// </summary>
+        private static void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+            
+        }
+
+        private static void ReceiveResponse()
+        {
+            var buffer = new byte[2048];
+            int received = ClientSocket.Receive(buffer, SocketFlags.None);
+            if (received == 0) return;
+            var data = new byte[received];
+            Array.Copy(buffer, data, received);
+            string text = Encoding.ASCII.GetString(data);
+            Console.WriteLine("");
+            Console.WriteLine("Response from client:");
+            Console.WriteLine(text);
+        }
+    }
+}
+    
+
